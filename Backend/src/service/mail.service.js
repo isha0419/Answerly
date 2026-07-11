5ddcacf87e7d0483e8d883a78a2ea070936b4719
isha0419 <isha01092004@gmail.com>
2026-07-11 13:29:34 +0530

    updated mail configuration and refresh token

--- Backend/src/service/mail.service.js
+++ Backend/src/service/mail.service.js
@@ -2,15 +2,17 @@ import nodemailer from "nodemailer"
 import "dotenv/config"
 
 const transporter = nodemailer.createTransport({
-    service: "gmail",
-    auth: {
-        type: 'OAuth2',
-        user: process.env.GOOGLE_USER,
-        clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-        refreshToken: process.env.GOOGLE_REFRESH_TOKEN,
-        clientId: process.env.GOOGLE_CLIENT_ID
-    }
-})
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
+  auth: {
+    type: "OAuth2",
+    user: process.env.GOOGLE_USER,
+    clientId: process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    refreshToken: process.env.GOOGLE_REFRESH_TOKEN,
+  },
+});
 
 transporter.verify()
   .then(() => {
